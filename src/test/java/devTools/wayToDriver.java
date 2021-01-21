cc23759d9284cabd6b4d93ff175633be56533b7f
MistaCheese <tylimovalex@gmail.com>
2021-01-21 16:38:30 +0300

    123

--- src/test/java/devTools/wayToDriver.java
+++ src/test/java/devTools/wayToDriver.java
@@ -5,7 +5,7 @@ import java.io.File;
 
 public class wayToDriver {
     static File file = new File("chromedriver.exe");
-    public static final String toFireFox = System.setProperty("webdriver.gecko.driver", "C:\\webDriver\\chrome\\88\\chromedriver.exe"); // путь до драйвера firefox
+    public static final String toFireFox = System.setProperty("webdriver.gecko.driver", file.getAbsolutePath()); // путь до драйвера firefox
     public static final String dbUrl = "jdbc:postgresql://116.203.111.44:5432/quidditch";
     public static final String dbLogin = "quidditch_user";
     public static final String dbPass = "quidditch_2021";
